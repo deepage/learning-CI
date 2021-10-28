6d0e057b0c7f79b5f9f8699c58b0a51df2be46fb
chao.tang <chao.tang@lynxi.com>
2021-10-28 15:10:04 +0800

    add test

---
+++ test/test_hello.cpp
@@ -0,0 +1,28 @@
+#include "Hello.h"
+#include <gtest/gtest.h>
+
+TEST(Hello, none) {
+    GTEST_ASSERT_EQ(1, 1);
+}
+
+TEST(Hello, int) {
+    std::vector<int> a{10, 20, 30};
+    std::vector<int> b{1, 2, 3};
+    int* c;
+    int ret = func_add<int>(a.data(), b.data(), a.size(), b.size(), c);
+    GTEST_ASSERT_EQ(ret, 0);
+}
+
+TEST(Hello, float) {
+    std::vector<float> a{10.f, 20.f, 30.f};
+    std::vector<float> b{1.f, 2.f, 3.f};
+    float* c;
+    int ret = func_add<float>(a.data(), b.data(), a.size(), b.size(), c);
+    GTEST_ASSERT_EQ(ret, 0);
+}
+
+int main(int argc, char* argv[])
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
